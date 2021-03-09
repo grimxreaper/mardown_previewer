279d94aeb89d55286753a37948d04638e1b945d5
Zahra <hizahrazadeh@gmail.com>
2021-03-09 11:15:28 -0500

    importing marked

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import marked from 'marked';
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      markdown: ""
+      text: ""
     }
   }
 
 handleChange = (event) => {
   //when text is entered into the editor element
   this.setState({
-    markdown: event.target.value //update preview element
+    text: event.target.value //update preview element
     
   })
 }
@@ -23,42 +24,32 @@ handleChange = (event) => {
 
 
   render() {
-    const { markdown } = this.state; //what does this do again?
+    const { text } = this.state; //what does this do again?
 
+    const markdown = marked(text);
     return(
     <div>
-      <h1 className="text-center mt-4">Convert your markdown!</h1>
+      <h1 className="text-center mt-4">Convert Your Markdown!</h1>
       <div className="row">
         <div className="col-6">
-          <h5>Enter your markdown here:</h5>
+          <h5 className="text-center">Enter your markdown here:</h5>
 
       <textarea
         className="form-control"
         id="editor"
-        value={markdown}
+        value={text}
         onChange={this.handleChange}
-
-        //trigger a change event here, and update the state of preview to render
-        //what was written in the editor as html
-
-      ></textarea>
+      />
       </div>
-      {/* When I enter text into the #editor element, the #preview element
-is updated as I type to display the content of the textarea. */}
-
-      {/* When I enter GitHub flavored markdown into the #editor element,
-the text is rendered as HTML in the #preview element as I type. */}
 
       <div
         id="preview"
-        className="col-6 preview"
+        className="col-6"
       >
-        <h5>See the result:</h5>
-        <div className="preview">
-          <p> dfsdfsdfsf </p>
+        <h5 className="text-center">See the result:</h5>
+        <div className="preview rounded">
+        <p> dfsdfsdfsf </p>
         </div>
-
-
       </div>
     </div>
     </div>
