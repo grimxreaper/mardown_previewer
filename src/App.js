1b37a6faa00dd774396c92ac8f0dafe2bc6881e6
Zahra <hizahrazadeh@gmail.com>
2021-03-09 10:35:38 -0500

    updated html

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import './App.css';
+import React, { Component } from 'react'
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props)
 
-    state = {
-      edit: true
+    this.state = {
+      markdown: ""
     }
   }
 
-handleChange = () => {
+handleChange = (e) => {
   //when text is entered into the editor element
-  this.setState(() => ({
-    edit: !state.edit //update preview element
+  this.setState({
+    markdown: e.target.value //update preview element
     
-  }))
+  })
 }
 
 
 
   render() {
+    const { markdown } = this.state; //what does this do again?
+
+    return(
     <div>
+      <div>Hello</div>
+      <h1>markdown previewer</h1>
+      <h2>convert your markdown!</h2>
       <textarea
         id="editor"
+        value={markdown}
+        onChange={this.handleChange}
 
         //trigger a change event here, and update the state of preview to render
         //what was written in the editor as html
@@ -39,13 +48,15 @@ the text is rendered as HTML in the #preview element as I type. */}
         {//result of preview
         }
 
-;
+
       </div>
     </div>
-  }
+
+    )}
+
 }
 
 
-
+// ReactDOM.render(<App/>, document.getElementById('app'))
 
 export default App;
